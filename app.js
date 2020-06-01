ad081405e5cff831257115dd86587c6006522f5c
Aakarsh Singh <aakarshsingh0909@gmail.com>
2020-06-01 09:27:43 +0530

    added port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const bodyParser=require("body-parser")
 const fetch = require("node-fetch");
-
+let port = process.env.PORT || 8080;
 const app=express()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
@@ -22,6 +22,6 @@ app.post('/post-test', (req, res) => {
       .then(obj => res.send(obj))
       });
 
-app.listen(8080,()=>{
+app.listen(port,()=>{
     console.log("The server is up and running")
 });
